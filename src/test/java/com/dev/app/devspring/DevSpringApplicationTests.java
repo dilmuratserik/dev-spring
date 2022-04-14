41f4fd0ae0e25b4c0ff1b596fe82371dde8b36f5
Dilmurat Serik <major@Air-Dilmurat.Dlink>
2022-04-15 02:29:01 +0600

    Initial commit

---
+++ src/test/java/com/dev/app/devspring/DevSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.app.devspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
